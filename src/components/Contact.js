604ae57f0f48ed7d11cedfd450b1ad57bc8cd34a
Mike790 <michael.kydd@outlook.com>
2024-01-17 14:58:02 -0500

    update contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,20 @@ function Contact() {
   return (
     <div>
       <h1>Contact</h1>
+      <form className="contact-form">
+        <label htmlFor="name">Enter your name</label>
+        <input className="contact-form-input" type="text" name="name" id="name" />
+
+        <label htmlFor="email">Enter your email</label>
+        <input className="contact-form-input" type="text" name="email" id="email" />
+
+        <label htmlFor="subject">Enter your subject</label>
+        <input className="contact-form-input" type="text" name="subject" id="subject" />
+        
+        <label htmlFor="message">Enter your message</label>
+        <input className="contact-form-input" type="text" name="message" id="message" />
+        <button type="submit">Send Mail</button>
+      </form>
     </div>
   )
 }
