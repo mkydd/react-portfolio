af4ef171d43ee5d00ed7e31e4ad04bd352b644ab
Mike790 <michael.kydd@outlook.com>
2024-01-19 15:11:44 -0500

    make certain input fields required

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,17 +22,17 @@ function Contact() {
     <div>
       <h1>Contact</h1>
       <form className="form" ref={form} onSubmit={sendEmail}>
-        <label htmlFor="name">Enter your name</label>
-        <input className="contact-form-input" type="text" name="name" id="name" />
+        <label htmlFor="name">Enter your name*</label>
+        <input className="contact-form-input" type="text" name="name" id="name" required />
 
-        <label htmlFor="email">Enter your email</label>
-        <input className="contact-form-input" type="text" name="email" id="email" />
+        <label htmlFor="email">Enter your email*</label>
+        <input className="contact-form-input" type="email" name="email" id="email" required />
 
         <label htmlFor="subject">Enter your subject</label>
-        <input className="contact-form-input" type="text" name="subject" id="subject" />
+        <input className="contact-form-input" type="text" name="subject" id="subject"  />
         
-        <label htmlFor="message">Enter your message</label>
-        <input className="contact-form-input" type="text" name="message" id="message" />
+        <label htmlFor="message">Enter your message*</label>
+        <input className="contact-form-input" type="text" name="message" id="message" required />
         <button type="submit">Send Mail</button>
       </form>
 
