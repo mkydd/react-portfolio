ff2b9f7cd0fbd5f0b95d1c5a95993814f495d790
Mike790 <michael.kydd@outlook.com>
2024-01-23 12:44:36 -0500

    update About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,21 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>ABOUT ME</h1>
+      {/* Insert Image of me here Maybe <img></img> */}
+      <h2>I am Michael Kydd</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Rerum doloribus libero quibusdam at quae, beatae unde rem a non 
+        suscipit eaque corrupti reprehenderit quasi autem!</p>
+      <div className="about-me-info">
+        <p><b>Full Name</b> : Michael Kydd</p>
+        <p><b>Nationality</b> : Canadian</p>
+        <p><b>Language(s)</b> : English</p>
+        <p><b>Location</b>: Hamilton, ON, Canada</p>
+        
+        <p><b>Working Status</b>: Seeking Employment</p>
+        <p>(Willing to Relocate)</p>
+      </div>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,9 @@ function Home() {
         <h1>Hi, I am <div>Michael Kydd</div></h1>
       </div>
       <div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum impedit sed unde excepturi vel explicabo quaerat quam quidem vero, rem commodi id amet ducimus optio?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Dolorum impedit sed unde excepturi vel explicabo quaerat quam 
+          quidem vero, rem commodi id amet ducimus optio?</p>
       </div>
       <div>
         <button>GitHub</button> {/* Use Material UI for GitHub logo */}
