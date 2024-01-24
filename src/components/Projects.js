6120b6edc2e5957874f600402407665112de72d2
Mike790 <michael.kydd@outlook.com>
2024-01-24 11:54:07 -0500

    update Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,65 @@
 import React from 'react'
 
 function Projects() {
+  const projects = [
+    {
+      name: 'React Calculator',
+      img: '',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa nostrum maxime praesentium! Libero, doloribus deserunt. Praesentium voluptates consectetur aliquam?',
+      url: 'https://',
+      tech: [
+        'ReactJS'
+      ]
+    }, 
+    {
+      name: 'Spotify Playlist Generator',
+      img: '',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa nostrum maxime praesentium! Libero, doloribus deserunt. Praesentium voluptates consectetur aliquam?',
+      url: 'https://',
+      tech: [
+        'ReactJS'
+      ]
+    }, 
+    {
+      name: 'Word Guessing Game',
+      img: '',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa nostrum maxime praesentium! Libero, doloribus deserunt. Praesentium voluptates consectetur aliquam?',
+      url: 'https://',
+      tech: [
+        'HTML',
+        'CSS',
+        'JavaScript'
+      ]
+    }, 
+    {
+      name: 'Final Grade Generator',
+      img: '',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa nostrum maxime praesentium! Libero, doloribus deserunt. Praesentium voluptates consectetur aliquam?',
+      url: 'https://',
+      tech: [
+        'Python',
+        'Tkinter (Python Package)'
+      ]
+    }
+
+  ]
+
   return (
     <div>
       <h1>Projects</h1>
+      <ul>
+        {projects.map(project => {
+          return (
+            <li key={project.name}>
+              <h2>{project.name}</h2>
+              <h3>Tech</h3>
+              <ul>
+                {project.tech.map(techItem => <li key={`${project.name}-${techItem}`}>{techItem}</li>)}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
