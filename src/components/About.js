7d01f310694f220482d895bba3d858cec0e27834
Mike790 <michael.kydd@outlook.com>
2024-01-24 23:02:24 -0500

    style About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import CVButton from './CVButton'
+import headshot from '../headshot.png'
 
 function About() {
   return (
-    <div>
-      <h1>ABOUT ME</h1>
-      {/* Insert Image of me here Maybe <img></img> */}
-      <h2>I am Michael Kydd</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Rerum doloribus libero quibusdam at quae, beatae unde rem a non 
-        suscipit eaque corrupti reprehenderit quasi autem!</p>
-      <div className="about-me-info">
-        <p><b>Full Name</b> : Michael Kydd</p>
-        <p><b>Nationality</b> : Canadian</p>
-        <p><b>Language(s)</b> : English</p>
-        <p><b>Location</b>: Hamilton, ON, Canada</p>
-        
-        <p><b>Working Status</b>: Seeking Employment</p>
-        <p>(Willing to Relocate)</p>
+    <div className='about-me'>
+      <div className="content">
+        <h1>ABOUT ME</h1>
+        <img src={headshot} alt='headshot' className='headshot' />
+        <h2>I am <span className='name'>Michael Kydd</span></h2>
+        <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Rerum doloribus libero quibusdam at quae, beatae unde rem a non 
+          suscipit eaque corrupti reprehenderit quasi autem!</p>
+        <div className="about-me-info">
+          <p><b>Full Name</b> : Michael Kydd</p>
+          <p><b>Nationality</b> : Canadian</p>
+          <p><b>Language(s)</b> : English</p>
+          <p><b>Location</b>: Hamilton, ON, Canada</p>
+          
+          <p><b>Working Status</b>: Seeking Employment</p>
+        </div>
+        <CVButton />
       </div>
-      <CVButton />
+      
     </div>
   )
 }
--- src/components/CVButton.js
+++ src/components/CVButton.js
@@ -5,7 +5,7 @@ function PdfDownloadComponent() {
   return (
     <div>
       <a href="/resume/kydd_resume.pdf" download="michael-kydd-resume">
-        <button type='download'>DOWNLOAD CV</button>
+        <button type='download' className='cv-download-button'>DOWNLOAD CV</button>
       </a>
     </div>
   );
