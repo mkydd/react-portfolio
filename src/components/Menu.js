f0392d072a1804a8df12ce0001fb5eb78b172394
Mike790 <michael.kydd@outlook.com>
2024-01-31 13:44:39 -0500

    style BurgerMenu: add footer

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,16 +8,19 @@ function Menu() {
       <div className="headshot">
         <img src={headshot} alt="headshot" srcset="" />
       </div>
-
-      <NavLink to='/'>Home</NavLink>
-      <br />
-      <NavLink to='/about'>About</NavLink>
-      <br />
-      <NavLink to='/resume'>Resume</NavLink>
-      <br />
-      <NavLink to='/projects'>Projects</NavLink>
-      <br />
-      <NavLink to='/contact'>Contact</NavLink>
+      <div className='navlink-box-wrapper'>
+        <div className="navlink-box">
+          <NavLink to='/' className='navlink'>Home</NavLink>
+          <NavLink to='/about' className='navlink'>About</NavLink>
+          <NavLink to='/resume' className='navlink'>Resume</NavLink>
+          <NavLink to='/projects' className='navlink'>Projects</NavLink>
+          <NavLink to='/contact' className='navlink'>Contact</NavLink>
+        </div>
+      </div>
+      <div className="footer">
+        <p>Made & Designed by</p>
+        <p className='name'>Michael Kydd</p>
+      </div>
     </div>
   )
 }
