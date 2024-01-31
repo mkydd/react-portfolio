fe1a449fc399ef63230751d52e0a5ef82619fe92
Mike790 <michael.kydd@outlook.com>
2024-01-31 11:25:35 -0500

    style Contact page: style contact info cards

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -7,17 +7,29 @@ function ContactInfo() {
   return (
     <div className='contact-info'>
       <div className="contact-info-card" style={{border: '1px solid black'}}>
-        <LocalPhoneOutlinedIcon />
+        <LocalPhoneOutlinedIcon 
+          style={{
+            border: '1px solid #cbced8',
+            padding: '0.25rem',
+            fontSize: '2rem'}} />
         <p className="contact-type">Phone</p>
         <p className="contact-info">+1 289-244-0493</p>
       </div>
       <div className="contact-info-card" style={{border: '1px solid black'}}>
-        <EmailOutlinedIcon />
+        <EmailOutlinedIcon 
+          style={{
+            border: '1px solid #cbced8',
+            padding: '0.25rem',
+            fontSize: '2rem'}} />
         <p className="contact-type">Email</p>
         <p className="contact-info">michael.kydd@outlook.com</p>
       </div>
       <div className="contact-info-card" style={{border: '1px solid black'}}>
-        <LocationOnOutlinedIcon />
+        <LocationOnOutlinedIcon 
+          style={{
+            border: '1px solid #cbced8',
+            padding: '0.25rem',
+            fontSize: '2rem'}} />
         <p className="contact-type">Location</p>
         <p className="contact-info">Hamilton, ON, Canada</p>
       </div>
