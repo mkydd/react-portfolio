14b8ef21cc4502f678b9a7feb89ad3be53a6abd8
Mike790 <michael.kydd@outlook.com>
2024-02-01 11:58:13 -0500

    close menu when link is clicked

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,22 +3,22 @@ import { NavLink } from 'react-router-dom'
 import headshot from '../imgs/headshot.png'
 import MenuButton from './MenuButton'
 
-function Menu( {handleClick} ) {
+function Menu( {handleCloseClick} ) {
   return (
     <div className='menu'>
       <div className="button-wrapper">
-        <MenuButton handleClick={handleClick}/>
+        <MenuButton handleCloseClick={handleCloseClick}/>
       </div>
       <div className="headshot">
         <img src={headshot} alt="headshot" srcset="" />
       </div>
       <div className='navlink-box-wrapper'>
         <div className="navlink-box">
-          <NavLink to='/' className='navlink'>Home</NavLink>
-          <NavLink to='/about' className='navlink'>About</NavLink>
-          <NavLink to='/resume' className='navlink'>Resume</NavLink>
-          <NavLink to='/projects' className='navlink'>Projects</NavLink>
-          <NavLink to='/contact' className='navlink'>Contact</NavLink>
+          <NavLink onClick={handleCloseClick} to='/' className='navlink'>Home</NavLink>
+          <NavLink onClick={handleCloseClick} to='/about' className='navlink'>About</NavLink>
+          <NavLink onClick={handleCloseClick} to='/resume' className='navlink'>Resume</NavLink>
+          <NavLink onClick={handleCloseClick} to='/projects' className='navlink'>Projects</NavLink>
+          <NavLink onClick={handleCloseClick} to='/contact' className='navlink'>Contact</NavLink>
         </div>
       </div>
       <div className="footer">
