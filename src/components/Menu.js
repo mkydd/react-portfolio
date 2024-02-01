23901173e5bc753df97abb46e3c2973df50d9129
Mike790 <michael.kydd@outlook.com>
2024-02-01 14:08:33 -0500

    style menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,27 +5,33 @@ import MenuButton from './MenuButton'
 
 function Menu( {handleCloseClick} ) {
   return (
-    <div className='menu'>
-      <div className="button-wrapper">
-        <MenuButton handleClick={handleCloseClick}/>
-      </div>
-      <div className="headshot">
-        <img src={headshot} alt="headshot" />
-      </div>
-      <div className='navlink-box-wrapper'>
-        <div className="navlink-box">
-          <NavLink onClick={handleCloseClick} to='/' className='navlink'>Home</NavLink>
-          <NavLink onClick={handleCloseClick} to='/about' className='navlink'>About</NavLink>
-          <NavLink onClick={handleCloseClick} to='/resume' className='navlink'>Resume</NavLink>
-          <NavLink onClick={handleCloseClick} to='/projects' className='navlink'>Projects</NavLink>
-          <NavLink onClick={handleCloseClick} to='/contact' className='navlink'>Contact</NavLink>
+    <div className="another-menu-wrapper">
+      <div className="menu-wrapper">
+        <div className='menu'>
+          <div className="headshot">
+            <img src={headshot} alt="headshot" />
+          </div>
+          <div className='navlink-box-wrapper'>
+            <div className="navlink-box">
+              <NavLink onClick={handleCloseClick} to='/' className='navlink'>Home</NavLink>
+              <NavLink onClick={handleCloseClick} to='/about' className='navlink'>About</NavLink>
+              <NavLink onClick={handleCloseClick} to='/resume' className='navlink'>Resume</NavLink>
+              <NavLink onClick={handleCloseClick} to='/projects' className='navlink'>Projects</NavLink>
+              <NavLink onClick={handleCloseClick} to='/contact' className='navlink'>Contact</NavLink>
+            </div>
+          </div>
+          <div className="footer">
+            <p>Designed & Developed by</p>
+            <p className='name'>Michael Kydd</p>
+          </div>
+        </div>
+        <div className="button-wrapper">
+          <MenuButton handleClick={handleCloseClick}/>
         </div>
       </div>
-      <div className="footer">
-        <p>Designed & Developed by</p>
-        <p className='name'>Michael Kydd</p>
-      </div>
     </div>
+    
+    
   )
 }
 
