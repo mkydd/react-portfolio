04871e9f72d874d1898ee800df1b4af5bf8f1604
Mike790 <michael.kydd@outlook.com>
2024-11-15 10:26:36 -0500

    update resume

--- src/components/CVButton.js
+++ src/components/CVButton.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function PdfDownloadComponent() {
-  
   return (
-    <div className='cv-download-button-wrapper'>
-      <a href="/resume/mkydd_resume.pdf" download="michael-kydd-resume">
-        <button type='download' className='cv-download-button'>DOWNLOAD CV</button>
+    <div className="cv-download-button-wrapper">
+      <a href="/resume/michael_kydd_resume.pdf" download="michael-kydd-resume">
+        <button type="download" className="cv-download-button">
+          DOWNLOAD CV
+        </button>
       </a>
     </div>
   );
-};
+}
 export default PdfDownloadComponent;
